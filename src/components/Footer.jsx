ecb9819b4a42e2d32771ccd2f4fd967bdcb3ba6c
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2022-11-19 18:17:25 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='py-5 text-center'>
+      <p className='text-sm mt-2 opacity-50 text-white'>&copy; {new Date().getFullYear()} Dipin Jayarajan. All rights reserved
+      </p>
+    </div>
   )
 }
 
